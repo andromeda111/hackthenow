80c67ce6d2a9219db1056d311669c4f172d8180a
John <jmseymour111@gmail.com>
2018-02-03 13:00:08 -0800

    created initial setup for api call to get movies

--- src/component/camera/index.js
+++ src/component/camera/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+let movieAPIKey = 'a6de91618bcf933ac45ea50b3a3eda26'
+let getMoviesRootURL = `https://api.themoviedb.org/3/discover/movie?api_key=${movieAPIKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 let capturedImage
 let highestEmo
 let emotions
@@ -27,10 +29,12 @@ class Camera extends React.Component {
 		})
 
 		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-				video.src = window.URL.createObjectURL(stream)
-				video.play()
-			})
+			navigator.mediaDevices
+				.getUserMedia({ video: true })
+				.then(stream => {
+					video.src = window.URL.createObjectURL(stream)
+					video.play()
+				})
 		}
 	}
 
@@ -45,14 +49,21 @@ class Camera extends React.Component {
 	emotions = blob => {
 		console.log('triggered emotions', blob)
 		$.ajax({
-			url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+			url:
+				'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
 			beforeSend: function(xhrObj) {
-				xhrObj.setRequestHeader('Content-Type', 'application/octet-stream')
-				xhrObj.setRequestHeader('Ocp-Apim-Subscription-Key', 'e7afc5c83a4e4ef8adb3859baf2d10a6')
+				xhrObj.setRequestHeader(
+					'Content-Type',
+					'application/octet-stream'
+				)
+				xhrObj.setRequestHeader(
+					'Ocp-Apim-Subscription-Key',
+					'e7afc5c83a4e4ef8adb3859baf2d10a6'
+				)
 			},
 			type: 'POST',
 			processData: false,
-			data: blob
+			data: blob,
 		})
 			.done(function(data) {
 				emotions = data[0].scores
@@ -80,23 +91,43 @@ class Camera extends React.Component {
 		sourceImg.append('sourceImg', blob)
 
 		$.ajax({
-			url: 'https://api.cognitive.microsoft.com/bing/v7.0/images/details?modules=RecognizedEntities',
+			url:
+				'https://api.cognitive.microsoft.com/bing/v7.0/images/details?modules=RecognizedEntities',
 			beforeSend: xhrObj => {
 				// Request headers
 				xhrObj.setRequestHeader('Content-Type', 'multipart/form-data')
-				xhrObj.setRequestHeader('Ocp-Apim-Subscription-Key', '3873387e25024b2ca2ec0ce5a31fe915')
+				xhrObj.setRequestHeader(
+					'Ocp-Apim-Subscription-Key',
+					'3873387e25024b2ca2ec0ce5a31fe915'
+				)
 			},
 			type: 'POST',
 			processData: false,
-			data: sourceImg
+			data: sourceImg,
 		}).done(res => {
-			let resultsArray = res.recognizedEntityGroups.value[1].recognizedEntityRegions[0].matchingEntities
+			let resultsArray =
+				res.recognizedEntityGroups.value[1].recognizedEntityRegions[0]
+					.matchingEntities
 
 			let matches = resultsArray.filter(result => {
 				return result.entity.jobTitle.toLowerCase().includes('act')
 			})
 
 			personResults = matches
+			console.log(personResults)
+			this.getMoviesWithPerson()
+		})
+	}
+
+	getMoviesWithPerson() {
+		let genres = '35'
+		let people = '3061'
+
+		$.ajax({
+			url: `${getMoviesRootURL}&with_genres=${genres}&with_cast=${people}`,
+			type: 'GET',
+		}).done(res => {
+			console.log(res)
 		})
 	}
 

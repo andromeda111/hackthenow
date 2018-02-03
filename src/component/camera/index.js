2da9e6a5b0cd4b13b18e29cadaee77bc26e9f402
Stephanie Dover <doverstephaniem@gmail.com>
2018-02-03 14:14:48 -0800

    fixed emotions function

--- src/component/camera/index.js
+++ src/component/camera/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import superagent from 'superagent'
 
+let genreMap = {
+	action: 28,
+	adventure: 12,
+	comedy: 35,
+	crime: 80,
+	documentary: 99,
+	drama: 18,
+	family: 10751,
+	fantasy: 14,
+	horror: 27,
+	mystery: 9648,
+	romance: 10749,
+	scienceFiction: 878,
+	thriller: 53,
+	war: 10752
+}
+let emotionMap = {
+	anger: [genreMap.crime, genreMap.action],
+	contempt: [genreMap.war, genreMap.action],
+	disgust: [genreMap.horror, genreMap.action],
+	fear: [genreMap.thriller],
+	happiness: [genreMap.romance, genreMap.family, genreMap.comedy, genreMap.adventure],
+	neutral: [genreMap.documentary, genreMap.scienceFiction, genreMap.family],
+	sadness: [genreMap.drama],
+	surprise: [genreMap.mystery, genreMap.fantasy, genreMap.adventure]
+}
 let movieAPIKey = 'a6de91618bcf933ac45ea50b3a3eda26'
 let getMoviesRootURL = `https://api.themoviedb.org/3/discover/movie?api_key=${movieAPIKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 let capturedImage
@@ -25,18 +51,15 @@ class Camera extends React.Component {
 			this.createImageBlob(canvas).then(blob => {
 				capturedImage = blob
 				this.emotions(blob)
-				console.log(capturedImage)
 				this.getSimilarPeople()
 			})
 		})
 
 		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices
-				.getUserMedia({ video: true })
-				.then(stream => {
-					video.src = window.URL.createObjectURL(stream)
-					video.play()
-				})
+			navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+				video.src = window.URL.createObjectURL(stream)
+				video.play()
+			})
 		}
 	}
 
@@ -49,37 +72,31 @@ class Camera extends React.Component {
 	}
 
 	emotions = blob => {
-		console.log('triggered emotions', blob)
 		$.ajax({
-			url:
-				'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
+			url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
 			beforeSend: function(xhrObj) {
-				xhrObj.setRequestHeader(
-					'Content-Type',
-					'application/octet-stream'
-				)
-				xhrObj.setRequestHeader(
-					'Ocp-Apim-Subscription-Key',
-					'e7afc5c83a4e4ef8adb3859baf2d10a6'
-				)
+				xhrObj.setRequestHeader('Content-Type', 'application/octet-stream')
+				xhrObj.setRequestHeader('Ocp-Apim-Subscription-Key', 'e7afc5c83a4e4ef8adb3859baf2d10a6')
 			},
 			type: 'POST',
 			processData: false,
-			data: blob,
+			data: blob
 		})
 			.done(function(data) {
 				emotions = data[0].scores
 				let keys = Object.keys(emotions)
+				console.log('emotions', emotions)
 				let highestVal = emotions[keys[0]]
-				let highest
+				let highest = keys[0]
 				for (let i = 1; i < keys.length; i++) {
 					if (emotions[keys[i]] > highestVal) {
 						highestVal = emotions[keys[i]]
 						highest = keys[i]
 					}
 				}
-				highestEmo = highest
-				console.log(highestEmo, emotions)
+				setTimeout(() => {
+					highestEmo = highest
+				}, 1500)
 			})
 			.fail(function(err) {
 				console.log('nope', err)
@@ -93,30 +110,23 @@ class Camera extends React.Component {
 		sourceImg.append('sourceImg', blob)
 
 		$.ajax({
-			url:
-				'https://api.cognitive.microsoft.com/bing/v7.0/images/details?modules=RecognizedEntities',
+			url: 'https://api.cognitive.microsoft.com/bing/v7.0/images/details?modules=RecognizedEntities',
 			beforeSend: xhrObj => {
 				// Request headers
 				xhrObj.setRequestHeader('Content-Type', 'multipart/form-data')
-				xhrObj.setRequestHeader(
-					'Ocp-Apim-Subscription-Key',
-					'3873387e25024b2ca2ec0ce5a31fe915'
-				)
+				xhrObj.setRequestHeader('Ocp-Apim-Subscription-Key', '3873387e25024b2ca2ec0ce5a31fe915')
 			},
 			type: 'POST',
 			processData: false,
-			data: sourceImg,
+			data: sourceImg
 		}).done(res => {
-			let resultsArray =
-				res.recognizedEntityGroups.value[1].recognizedEntityRegions[0]
-					.matchingEntities
+			let resultsArray = res.recognizedEntityGroups.value[1].recognizedEntityRegions[0].matchingEntities
 
 			let matches = resultsArray.filter(result => {
 				return result.entity.jobTitle.toLowerCase().includes('act')
 			})
 
 			personResults = matches
-			console.log(personResults)
 			this.getPersonId()
 		})
 	}
@@ -142,13 +152,13 @@ class Camera extends React.Component {
 	}
 
 	getMoviesWithPerson() {
-		let genres = '35|28'
-		// let people = ids.join('|')
+		console.log(highestEmo)
+		let genres = emotionMap[highestEmo].join('|')
 		let results = []
 		ids.map(id => {
 			$.ajax({
 				url: `${getMoviesRootURL}&with_genres=${genres}&with_cast=${id}`,
-				type: 'GET',
+				type: 'GET'
 			}).done(res => {
 				results.push(res.results[0])
 			})

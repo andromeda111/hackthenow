02dbeb83df860721ee72878763b4e958da374c99
John Seymour <jmseymour111@gmail.com>
2018-02-03 15:26:58 -0800

    starting layout for actor area (#8)

--- src/component/actors/index.js
+++ src/component/actors/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
 
-class Actors extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
+let imageURL = 'https://image.tmdb.org/t/p/w500'
 
-  render () {
-    let actors = this.props.actors ? this.props.actors : []
-    return (
-      <div className='actors'>
-        <div />
-      </div>
-    )
-  }
+class Actors extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        let actors = this.props.actors ? this.props.actors : []
+        console.log(actors)
+        return (
+            <div className="actors">
+                {actors.map((actor, idx) => {
+                    return (
+                        <div key={idx} className="actor-container">
+                            <div className="actor-img">
+                                <img
+                                    src={actor.entity.url}
+                                    height="500"
+                                    width="500"
+                                />
+                            </div>
+                            <div>{actor.entity.name}</div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default Actors

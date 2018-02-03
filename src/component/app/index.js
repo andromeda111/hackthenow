115bc7d206a9061f60e73e6fa8dd1aab4467ffcb
Stephanie Dover <doverstephaniem@gmail.com>
2018-02-03 08:59:23 -0800

    fixing things

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import Landing from '../landing'
 
 class App extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-		}
+		this.state = {}
+	}
+
+	onChange = e => {
+		e.preventDefault()
 	}
 
 	render() {
 		return (
 			<div className="app">
+				<input type="file" onChange={e => this.onChange(e)} />
 			</div>
 		)
 	}
